dc7837840ef0776c1b861bf27d6f117f8ca8a76a
Ricky Almeida <ricky.almeida.dev@gmail.com>
2020-08-19 23:36:02 -0300

    feat: list projects from api

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
-import React from 'react';
-import {View, Text, StyleSheet, StatusBar} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {FlatList, Text, StyleSheet, StatusBar, View} from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await api.get('projects');
+      setProjects(response.data);
+    })();
+  }, []);
+
   return (
-    <>
+    <View style={styles.container}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="#7159c1"
         translucent
       />
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello from React Native!</Text>
-      </View>
-    </>
+
+      <FlatList
+        style={styles.list}
+        data={projects}
+        keyExtractor={(project) => project.id}
+        renderItem={({item: project}) => (
+          <Text style={styles.title}>{project.title}</Text>
+        )}
+      />
+    </View>
   );
 }
 
@@ -23,9 +40,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  list: {
+    marginTop: 40,
+  },
   title: {
     color: '#f5f5f5',
-    fontSize: 20,
+    fontSize: 32,
     fontWeight: 'bold',
   },
 });
